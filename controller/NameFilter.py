c83dcf14523ea87c25dd74751a9e6fa4c8bc38f5
josepserra20 <jserravives@cifpfbmoll.eu>
2022-03-13 13:18:17 +0100

    feat: :sparkles: request methods
    add request methods

--- controller/NameFilter.py
+++ controller/NameFilter.py
@@ -1,7 +1,19 @@
-from flask import render_template
+from flask import render_template, request
 from services.services import Services
 
 class NameFilter:
 
+    @staticmethod
+    def resolve(name):
+        if request.method == "GET":
+            return NameFilter.get(name)
+        if request.method == "DELETE":
+            return NameFilter.delete(name)
+
+    @staticmethod
     def get(name):
         return render_template('inventarioName.html', lista=Services.getName(name))
+
+    @staticmethod
+    def delete(name):
+        return Services.DeleteByName(name)
