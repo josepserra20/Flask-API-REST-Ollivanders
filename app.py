c0d92ac3e46b4451167f3012ed9ce51bae29260a
josepserra20 <jserravives@cifpfbmoll.eu>
2022-03-13 13:15:31 +0100

    feat: routes
    add new routes

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ from flask import Flask
 from controller.Inventario import Inventario
 from controller.index import Index
 from controller.NameFilter import NameFilter
+from controller.CreateDoc import Create
 
 app = Flask(__name__)
 
 app.add_url_rule('/','',Index.get)
-
 app.add_url_rule('/inventario','inventario',Inventario.get)
-
-app.add_url_rule('/inventario/<name>','name',NameFilter.get)
-
-# app.route_url_rule('deletebyname/<name>', 'delete', )
+app.add_url_rule('/inventario/<name>','name',NameFilter.resolve, methods=['GET', 'DELETE'])
+app.add_url_rule('/inventario/<name>/<quality>/<sell_in>', 'create',Create.resolve, methods=['POST'])
     
