31c38aad02276b6d23936b3d9ed34ce0ef62c720
josepserra20 <jserravives@cifpfbmoll.eu>
2022-03-19 19:04:19 +0100

    feat:

--- tests/test_index.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-def test_index(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"<h1>This is the main page of ollivaders API REST</h1>" in response.data
